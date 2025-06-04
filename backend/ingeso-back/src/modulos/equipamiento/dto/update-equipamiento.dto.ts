bf74b1e517c1688fd2f8e21e0aeded820e26b090
PA1IN <d.ramosguerra018@gmail.com>
2025-06-04 18:36:48 -0400

    ingeso-back

--- backend/ingeso-back/src/modulos/equipamiento/dto/update-equipamiento.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateEquipamientoDto } from './create-equipamiento.dto';
-
-export class UpdateEquipamientoDto extends PartialType(CreateEquipamientoDto) {}
