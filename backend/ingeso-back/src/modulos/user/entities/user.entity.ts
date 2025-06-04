43e43fd75a2d6c6786900d643d84cc1859e75fb3
LosJimy <beo.erazoo@gmail.com>
2025-06-04 19:30:20 -0400

    a

--- backend/ingeso-back/src/modulos/user/entities/user.entity.ts
+++ backend/ingeso-back/src/modulos/user/entities/user.entity.ts
@@ -10,10 +10,10 @@ export class User {
   @Column({ unique: true })
   rut: string;
 
-  @Column({ name: 'contraseña' })
+  @Column({ name: 'nombre_usuario' })
   password: string;
 
-  @Column({ name: 'nombre_usuario' })
+  @Column({ name: 'contraseña' })
   nombre: string;
 
   @Column()
